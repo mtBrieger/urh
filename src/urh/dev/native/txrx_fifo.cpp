f84665682c917004ae2991bf3388cdb901eff918
mtBrieger <developer@brieg.eu>
2019-06-11 15:54:49 +0200

    Update txrx_fifo.cpp

--- src/urh/dev/native/txrx_fifo.cpp
+++ src/urh/dev/native/txrx_fifo.cpp
@@ -3,7 +3,6 @@
  use 'sudo rm /tmp/tx_fifo; rm /tmp/rx_fifo' to remove fifos and stop process
  */
 
-#include "wavetable.hpp"
 #include <uhd/exception.hpp>
 #include <uhd/types/tune_request.hpp>
 #include <uhd/usrp/multi_usrp.hpp>
